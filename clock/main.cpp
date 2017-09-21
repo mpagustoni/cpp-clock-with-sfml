64a362f13c6a98fc2e70993ea41aff47814a1177
Unknown <mpagustoni@inf.ufrgs.br>
2017-09-21 11:21:40 -0300

    added pointers
    initial implementation of pointers

--- clock/main.cpp
+++ clock/main.cpp
@@ -13,7 +13,23 @@ int main()
     }
     sf::Sprite clockSprite(clockTexture);
     clockSprite.setScale(windowSize.x / clockSprite.getLocalBounds().width, windowSize.y / clockSprite.getLocalBounds().height);
+    cout << windowSize.x << endl;
 
+    sf::Vector2u pointerPos(98, 98);
+
+    sf::RectangleShape secPointer(sf::Vector2f(5, (windowSize.y / 2) - 10));
+    secPointer.setPosition(pointerPos.x, pointerPos.y);
+    secPointer.setFillColor(sf::Color::Red);
+
+    sf::RectangleShape minPointer(sf::Vector2f(5, (windowSize.y / 2) - 10));
+    minPointer.setPosition(pointerPos.x, pointerPos.y);
+    minPointer.setFillColor(sf::Color::Black);
+
+    minPointer.rotate(270);
+
+    sf::RectangleShape hourPointer(sf::Vector2f(sf::Vector2f(5, (windowSize.y / 2) - 25)));
+    hourPointer.setPosition(pointerPos.x, pointerPos.y);
+    hourPointer.setFillColor(sf::Color::Black);
 
     while (window.isOpen())
     {
@@ -25,7 +41,11 @@ int main()
         }
 
         window.clear();
+
         window.draw(clockSprite);
+        window.draw(secPointer);
+        window.draw(minPointer);
+        window.draw(hourPointer);
         window.display();
     }
 
